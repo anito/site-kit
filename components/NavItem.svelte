2b18a1ef50df3d8a151670dcfe0df072bd63e91d
anito <axelnitzschner@mac.com>
2020-11-27 22:34:09 +0100

    2018

--- components/NavItem.svelte
+++ components/NavItem.svelte
@@ -1,19 +1,23 @@
 <script>
-	import { getContext } from 'svelte';
+  import { getContext } from "svelte";
 
-	export let segment = null;
-	export let external = null;
-	export let blank = false;
-	export let title = null;
-	export let slug = '';
+  export let segment = null;
+  export let external = null;
+  export let blank = false;
+  export let title = null;
+  export let slug = "";
 
-	const current = getContext('nav');
+  const current = getContext("nav");
 
-	$: href = slug ? `${segment}/${slug}` : segment;
+  $: href = segment && slug ? `${segment}/${slug}` : segment || slug;
 </script>
 
 {#if external}
-	<li><a target={blank ? '_blank' : ''} href={external} {title}><slot></slot></a></li>
+  <li>
+    <a target={blank ? '_blank' : ''} href={external} {title}><slot /></a>
+  </li>
 {:else}
-	<li class:active="{$current === segment}"><a rel="prefetch" {href} {title}><slot></slot></a></li>
+  <li class:active={$current === segment}>
+    <a rel="prefetch" {href} {title}><slot /></a>
+  </li>
 {/if}
