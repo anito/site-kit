5e987fe1f43eea9288681299ab37a251d09207ad
anito <axelnitzschner@mac.com>
2020-11-28 03:36:48 +0100

    improved NavItems

--- components/NavItem.svelte
+++ components/NavItem.svelte
@@ -1,23 +1,40 @@
 <script>
-  import { getContext } from "svelte";
+  import { getContext, onMount } from "svelte";
 
   export let segment = null;
+  export let link = null;
   export let external = null;
   export let blank = false;
   export let title = null;
-  export let slug = "";
 
   const current = getContext("nav");
 
-  $: href = segment && slug ? `${segment}/${slug}` : segment || slug;
+  let anchor;
+  let href;
+
+  onMount(() => {
+    anchor.addEventListener("mouseover", createRef);
+
+    return () => anchor.removeEventListener("mouseover", createRef);
+  });
+
+  function createRef() {
+    href = link || (active && window.location.href) || segment;
+  }
+
+  $: active = $current === segment;
 </script>
 
 {#if external}
   <li>
-    <a target={blank ? '_blank' : ''} href={external} {title}><slot /></a>
+    <a
+      bind:this={anchor}
+      target={blank ? '_blank' : ''}
+      href={external}
+      {title}><slot /></a>
   </li>
 {:else}
-  <li class:active={$current === segment}>
-    <a rel="prefetch" {href} {title}><slot /></a>
+  <li class:active>
+    <a bind:this={anchor} rel="prefetch" {href} {title}><slot /></a>
   </li>
 {/if}
